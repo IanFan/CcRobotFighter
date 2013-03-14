9a9776a751d9531a72cb86943c08f6963993b64e
IanFan <ianfan0704@gmail.com>
2013-03-14 10:51:59 +0800

    Initial Commit

---
+++ RobotFighter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RobotFighter
+//
+//  Created by Ian Fan on 14/03/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
